45cf9f70014b52d15b44097f110b7be703521f62
JijoAndrews <jijoandrews07@gmail.com>
2024-08-31 08:30:31 +0530

    bootstrap updated again

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 import App from './App.jsx'
